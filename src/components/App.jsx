7f538b37459fd4b990c86d1d30fd5a7098ac5c6b
lmwang27 <52217891+lmwang27@users.noreply.github.com>
2021-01-28 18:18:09 -0800

    adding a form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
+
+  function handleClick(event) {
+    setHeadingText(name);
+    event.preventDefault();
+  }
+
+  function handleOnchange(event) {
+    console.log(event.target.value);
+    console.log(event.target.placeholder);
+    console.log(event.target.type);
+    setName(event.target.value);
+  }
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1> Hello {headingText}</h1>
+      <form action="">
+        <input
+          onChange={handleOnchange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit" onClick={handleClick}>
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
